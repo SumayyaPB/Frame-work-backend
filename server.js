048883e3fdd32ec6c361cdce0fe9ed3f273a1732
dell <summaya.pb@trustwaves.tech>
2025-12-10 00:00:48 +0530

    remove cloudinary variables

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(cors());
 
 //  Cloudinary config
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 //  Multer Cloudinary storage setup
